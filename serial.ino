c3544deaf6f59e71654b06a03a356b5b912022ba
apospraf <apospraf1@hotmail.com>
2019-06-09 20:42:59 +0300

    Final file

--- serial.ino
+++ serial.ino
@@ -2,24 +2,32 @@ int serialData[5];
 int A=11;
 int B=10;
 int C=9;
+int D=6;
+int E=5;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(A, OUTPUT);
   pinMode(B, OUTPUT);
   pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
   Serial.begin(9600);
+  for (int i=0; i<5; i++){
+    serialData[i] = 0;
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available() >=3) {
-    for (int i=0; i<=5; i++){
-      serialData[i] = Serial.read();      
+  while (Serial.available() >=5) {
+    for (int i=0; i<5; i++){
+      serialData[i] = Serial.read();
     }
     analogWrite(A, serialData[0]);
     analogWrite(B, serialData[1]);
     analogWrite(C, serialData[2]);
-
+    analogWrite(D, serialData[3]);
+    analogWrite(E, serialData[4]); 
   }
 }
